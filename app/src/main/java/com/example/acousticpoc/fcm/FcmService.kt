94f447d9fc0f97dc5cf21430dc254b7d7944b100
Daniel Ramirez <zet.daniel@outlook.com>
2021-01-29 19:58:09 -0600

    Micro Refactor

--- app/src/main/java/com/example/acousticpoc/fcm/FcmService.kt
+++ app/src/main/java/com/example/acousticpoc/fcm/FcmService.kt
@@ -1,14 +1,11 @@
-package com.example.acousticpoc
+package com.example.acousticpoc.fcm
 
 import co.acoustic.mobile.push.sdk.api.MessagingApi
 import co.acoustic.mobile.push.sdk.api.fcm.FcmApi
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
-
-
-
- class MyFirebaseMessagingService: FirebaseMessagingService() {
+ class FcmService: FirebaseMessagingService() {
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         if(FcmApi.isFcmMessage(remoteMessage)) {
             FcmApi.handleMceFcmMessage(getApplicationContext(), remoteMessage);
